5a7793afc4b5a3f3d91c3236b40cce88363156e1
Anderson Silva Viana <anviana@uolinc.com>
2020-03-23 04:14:35 -0300

    ZED005 - Test Api Runner

--- src/test/java/Runner/TestApiRunner.java
+++ src/test/java/Runner/TestApiRunner.java
@@ -19,37 +19,57 @@ import zeDelivery.api.obj.WeatherResponse;
  * : $
  * @since 23/03/2020 02:03
  */
-public class TesteRunner {
+public class TestApiRunner {
 
     private RequestSpecification request = given();
     private Response response;
+    final String ENDPOINT = "http://api.openweathermap.org/data/2.5/weather";
+    final String API_KEY ="3fda1d11f5903b756fbbff896133556c";
+    final String CIDADE ="BARUERI";
 
     @Test
-    public void getCidade() {
-
-          String ENDPOINT = "http://api.openweathermap.org/data/2.5/weather";
-          String API_KEY ="3fda1d11f5903b756fbbff896133556c";
-          String CIDADE ="BARUERI";
-
+    public void getCidade200() {
 
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
-
         request.baseUri(ENDPOINT);
         request.param("appid", API_KEY);
         request.param("q", CIDADE);
         request.header("Content-Type", "application/json");
         request.urlEncodingEnabled(true);
-
         response = request
                 .when()
                 .get(ENDPOINT);
-
         response.then().statusCode(HttpStatus.SC_OK);
-
         WeatherResponse wr = new WeatherResponse();
         wr = gson.fromJson(response.getBody().asString(), WeatherResponse.class);
-
     }
+
+    @Test
+    public void getCidade401(){
+        request.baseUri(ENDPOINT);
+        request.header("Content-Type", "application/json");
+        request.param("q",CIDADE);
+        request.param("appid","invalido");
+        request.urlEncodingEnabled(true);
+        response = request
+                .when()
+                .get(ENDPOINT);
+        response.then().statusCode(401);
+    }
+
+    @Test
+    public void getCidade404(){
+        request.baseUri(ENDPOINT);
+        request.header("Content-Type", "application/json");
+        request.param("q","invalida");
+        request.param("appid",API_KEY);
+        request.urlEncodingEnabled(true);
+        response = request
+                .when()
+                .get(ENDPOINT);
+        response.then().statusCode(HttpStatus.SC_NOT_FOUND);
+    }
+
 
 
 }
